b923afb74b7f49aa2e909a53edbfe1064187c4e2
Eric Russell <Erictim.Russell@live.com>
2023-03-14 09:47:41 -0600

    loop through and display region cards

--- src/components/RegionCard.vue
+++ src/components/RegionCard.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="bg-tan elevation-5 p-1 rounded">
+  <div class="bg-light elevation-5 p-1 rounded">
     <div>
-      <h3 class="text-center">Region #</h3>
-      <div class="text-center d-flex flex-column">
-        <span class="fs-lg">0</span>
-        <span class="fs-lg">0</span>
-        <span class="fs-lg">0</span>
+      <h3 class="text-center">Region # {{ regions.regionNumber }}</h3>
+      <div class="text-center d-flex flex-column gap-3">
+        <span class="fs-xl material-symbols-outlined text-warning">attach_money</span>
+        <span class="fs-xl text-secondary material-symbols-outlined">factory</span>
+        <span class="fs-xl text-success material-symbols-outlined">psychiatry</span>
       </div>
     </div>
-    <div class="text-end m-3">
-      <button class="p-2" title="Delete?"><span class="material-symbols-outlined fs-md">
+    <div class="text-end pt-3 m-3">
+      <button @click="deleteRegion()" class="p-2" title="Delete?"><span class="material-symbols-outlined fs-md">
           delete_forever
         </span></button>
     </div>
@@ -17,16 +17,21 @@
 </template>
 
 <script>
-import { Region } from "../models/Region";
 
 export default {
-  // props: {
-  //   regions: { type: Region, required: true },
-  // },
+  props: {
+    regions: { type: Object, required: true },
+  },
   setup(props) {
 
     return {
+      async deleteRegion() {
+        try {
 
+        } catch (error) {
+
+        }
+      }
     }
   }
 }
