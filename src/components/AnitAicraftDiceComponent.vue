044d54fc1bf52d08f4344cac23a9adf19f5feee3
Eric Russell <Erictim.Russell@live.com>
2023-04-03 19:26:16 -0600

    point defense item for AA dice

--- src/components/AnitAicraftDiceComponent.vue
+++ src/components/AnitAicraftDiceComponent.vue
@@ -3,13 +3,17 @@
   <div class="col-12 text-center pt-5">
     <h2>Select Adversary</h2>
   </div>
-  <div class="col-12 d-flex justify-content-center py-2">
+  <div class="col-12 d-flex justify-content-center">
     <input class="mx-1 checkbox" type="checkbox" v-model="aircraft">
     <h3>Aircraft</h3>
   </div>
-  <div class="col-12 d-flex text-center justify-content-center">
+  <div class="col-12 d-flex text-center justify-content-center py-2">
     <input class="mx-1 checkbox" type="checkbox" v-model="missile">
     <h3>Missile</h3>
+  </div>
+  <div class="col-12 d-flex text-center justify-content-center">
+    <input class="mx-1 checkbox" type="checkbox" v-model="pointDefense">
+    <h3>Point Defense</h3>
   </div>
 
   <!-- Vs Aircraft -->
@@ -65,6 +69,32 @@
       </div>
     </div>
 
+    <!-- With Point Defense -->
+    <div class="col-3 d-flex justify-content-center mt-5" for="pointDefense" v-if="pointDefense == true">
+      <div class="text-center" v-if="(oneTwelveDie.roll >= 1) && (oneTwelveDie.roll <= 8)">
+        <h6>Roll Dice</h6>
+        <button :disabled="isPending" type="button" @click="rollOneTwelveDie()" class="btn p-5">
+          <span v-if="!isPending" class="fs-lg">{{ oneTwelveDie.roll }}</span>
+          <div v-if="isPending" class="spinner-border py-4" role="status">
+            <span></span>
+          </div>
+        </button>
+        <h3 v-show="oneTwelveDie.roll > 0" v-if="!isPending" class="text-center pt-3 text-success">Success</h3>
+        <h3 v-show="oneTwelveDie.roll > 0" v-if="isPending" class="text-center pt-3">Rolling...</h3>
+      </div>
+      <div class="text-center" v-else>
+        <h6>Roll Dice</h6>
+        <button :disabled="isPending" type="button" @click="rollOneTwelveDie()" class="btn p-5">
+          <span v-if="!isPending" class="fs-lg">{{ oneTwelveDie.roll }}</span>
+          <div v-if="isPending" class="spinner-border py-4" role="status">
+            <span></span>
+          </div>
+        </button>
+        <h3 v-show="oneTwelveDie.roll > 0" v-if="!isPending" class="text-center pt-3 text-danger">Fail</h3>
+        <h3 v-show="oneTwelveDie.roll > 0" v-if="isPending" class="text-center pt-3">Rolling...</h3>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -77,10 +107,12 @@ export default {
     const isPending = ref(false)
     const aircraft = ref(false)
     const missile = ref(false)
+    const pointDefense = ref(false)
 
     return {
       aircraft,
       missile,
+      pointDefense,
       oneTwelveDie,
       isPending,
       rollOneTwelveDie() {
