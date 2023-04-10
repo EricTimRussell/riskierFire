f5dbbbe6e19dea7d3e71bd8433422011229e43b9
Eric Russell <Erictim.Russell@live.com>
2023-04-10 10:46:00 -0600

    removing draf n drop features

--- src/views/ConstructionView.vue
+++ src/views/ConstructionView.vue
@@ -6,11 +6,9 @@
         <button class="m-2" data-bs-toggle="modal" data-bs-target="#construction-modal"
           aria-label="Start Construction">+Building</button>
       </div>
-      <div v-for="c in construction">
-        <ConstructionCardComponent :construction="c" :teams="teams" />
-      </div>
     </div>
   </div>
+
 
   <ModalComponent id="construction-modal">
     <ConstructBuildingComponent :key="user?.uid" :teams="teams" />
@@ -27,34 +25,9 @@ import { buildingsService } from "../services/BuildingsService";
 import { useRegionStore } from "../stores/RegionStore";
 import ConstructBuildingComponent from "../components/ConstructBuildingComponent.vue";
 import { useConstructionStore } from "../stores/ConstructionStore";
-import ConstructionCardComponent from "../components/ConstructionCardComponent.vue";
 export default {
   setup() {
     const user = useCurrentUser();
-
-    const items = ref([
-      { id: 0, title: 'Factory', list: 1 },
-      { id: 1, title: 'Aifield', list: 1 },
-      { id: 2, title: 'Naval Yard', list: 2 },
-      { id: 3, title: 'Naval Yard', list: 3 },
-    ])
-
-    const getList = (list) => {
-      return items.value.filter((item) => item.list == list)
-    }
-
-    const startDrag = (event, item) => {
-      event.dataTransfer.dropEffect = 'move'
-      event.dataTransfer.effectAllowed = 'move'
-      event.dataTransfer.setData('itemID', item.id)
-    }
-
-    const onDrop = (event, list) => {
-      const itemID = event.dataTransfer.getData('itemID')
-      const item = items.value.find((item) => item.id == itemID)
-      // @ts-ignore
-      item.list = list
-    }
 
     async function getTeamByUserId() {
       try {
@@ -91,9 +64,6 @@ export default {
       teams: computed(() => useRegionStore.teams),
       construction: computed(() => useConstructionStore.construction),
       user,
-      getList,
-      startDrag,
-      onDrop,
 
       async constructShip() {
         try {
@@ -104,27 +74,8 @@ export default {
       }
     }
   },
-  components: { ModalComponent, ConstructBuildingComponent, ConstructionCardComponent }
+  components: { ModalComponent, ConstructBuildingComponent }
 }
 </script>
 
-<style scoped>
-.drop-zone {
-  background-color: aliceblue;
-  padding: 20px;
-  border: 5px black solid;
-}
-
-.drag-el {
-  background-color: #2dafff;
-  color: black;
-  font-size: 16px;
-  padding: 5px;
-  width: fit-content;
-  margin-bottom: 10px;
-}
-
-.drag-el:nth-last-of-type(1) {
-  margin-bottom: 0px;
-}
-</style>
+<style scoped></style>
